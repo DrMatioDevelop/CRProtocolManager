c5b4995cc16cdc5886aa560c9a2a3979732baef9
cyfgit <cyfsoftware@126.com>
2017-03-17 10:26:47 +0800

    Initial Commit

---
+++ CRProtocolManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRProtocolManager
+//
+//  Created by d2c_cyf on 17/3/17.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

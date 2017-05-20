cecf217678b85539271f3191d856efa5ffc8a254
d2c_cyf <cyfsoftware@126.com>
2017-05-20 14:14:19 +0800

    注释

--- CRProtocolManager/CRProtocolManager.h
+++ CRProtocolManager/CRProtocolManager.h
@@ -9,7 +9,20 @@
 #import <Foundation/Foundation.h>
 
 @interface CRProtocolManager : NSObject
+/**
+ 协议管理  注册一个协议
+
+ @param provide  协议提供者
+ @param protocol 协议
+ */
 + (void)registServiceProvide:(id)provide forProtocol:(Protocol *)protocol;
 
+
+/**
+ 协议管理 获得一个协议
+
+ @param protocol 协议的名称
+ @return 协议
+ */
 + (id)serviceProvideForProtocol:(Protocol *)protocol;
 @end
